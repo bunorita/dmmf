cfcfae1ea9022d72c6985871a3efe8aa52318f13
Buntaro Orita <buntaro.orita@gmail.com>
2023-04-03 12:35:56 +0900

    Run PlaceOrder workflow

--- Main.fs
+++ Main.fs
@@ -1,8 +1,34 @@
-﻿open OrderTaking.Common
+﻿open OrderTaking.PlaceOrder
+open OrderTaking.PlaceOrder.Implementation
 
 [<EntryPoint>]
 let main argv =
-    printfn "done"
+    let addr =
+        { AddressLine1 = "1190 Benedum Drive"
+          AddressLine2 = "Narrowsburg"
+          AddressLine3 = ""
+          AddressLine4 = ""
+          City = "NY"
+          ZipCode = "12764" }
+
+    let customerInfo =
+        { FirstName = "John"
+          LastName = "Lennon"
+          EmailAddress = "john@beatles.com" }
+
+    let unvalidatedOrder: UnvalidatedOrder =
+        { OrderId = "foo"
+          CustomerInfo = customerInfo
+          ShippingAddress = addr
+          BillingAddress = addr
+          Lines =
+            [ { OrderLineId = "foo"
+                ProductCode = "W1234"
+                Quantity = 1.0M } ] }
+
+    let eventList = placeOrder unvalidatedOrder
+    eventList |> List.iter (fun event -> printfn "event: %A" event)
+    printfn "done."
     0
 // let fiveKilos = 5.0<kg>
 // let fiveMeters = 5.0<m>
